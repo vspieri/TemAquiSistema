950afefbf17c9af0a586a4e6a9a3ea2fa37b9fe0
vspieri <viniciuspieri1632@gmail.com>
2024-06-20 10:21:52 -0300

    vai biscate

--- Migrations/20240620131847_Criacao-TUDO.Designer.cs
+++ Migrations/20240620131847_Criacao-TUDO.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tem_Aqui.Models;
 
@@ -11,9 +12,11 @@ using Tem_Aqui.Models;
 namespace Tem_Aqui.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240620131847_Criacao-TUDO")]
+    partial class CriacaoTUDO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
